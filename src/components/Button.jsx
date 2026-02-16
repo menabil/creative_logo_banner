95f19a73e854309c0b3f85782e10e32e8b42d09d
meNabil <nabil99cc@gmail.com>
2026-02-16 15:42:33 +0600

    done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
+const Button = ({ btnText, className }) => {
+  return (
+    <button
+      className={`py-3.5 px-7 rounded-[100px] bg-red-500 text-white hover:text-white hover:bg-black  duration-500 ${className}`}
+    >
+      {btnText}
+    </button>
+  );
+};
 
-
-const Button = ({btnText , className}) => {
-    return (
-        <button className={`py-3.5 px-7 rounded-[100px] bg-[#FF7628] text-white border border-[#FF7628] hover:bg-white hover:text-[#FF7628] duration-500 ${className}`}>{btnText}</button>
-    )
-}
-
-export default Button
+export default Button;
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -4,10 +4,38 @@ import Flex from "../Flex";
 import Images from "../Images";
 import logo from "/src/assets/logo.png";
 import Button from "../Button";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdMail } from "react-icons/md";
 
 const Header = () => {
   return (
     <>
+      {/* Top header  */}
+      <header className="bg-red-500">
+        <Container>
+          <Flex className={"justify-between py-2"}>
+            <Flex className={"text-white gap-x-3 items-end"}>
+              <p className="text-xl">Admission Going on</p>
+              <FaPhoneAlt className="text-lg" />
+              <p className="font-semibold cursor-pointer">01791655368</p>
+              <MdMail className="font-sans font-bold" />
+              <p className="lg:font-semibold font-medium cursor-pointer">
+                mr.nabil.dev@gmail.com
+              </p>
+            </Flex>
+
+            <div className="">
+              <button className="cursor-pointer bg-white text-red-500 py-1 px-1.5 rounded-[6px] flex items-center lg:font-bold text-[10px]">
+                BN
+                <p className="bg-red-500 text-white py-px px-1 rounded-sm ml-3">
+                  EN
+                </p>
+              </button>
+            </div>
+          </Flex>
+        </Container>
+      </header>
+      {/* Top header  */}
       <Container className={"py-5"}>
         <Flex className={"justify-between"}>
           <Link to={"/"}>
